e75082614c4c7cf9416caabd365e5489d59c6f17
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2021-02-02 20:32:30 -0700

    New defaults surrounding server timeouts.

--- config.go
+++ config.go
@@ -11,17 +11,21 @@ import (
 )
 
 type configuration struct {
-	Context         context.Context
-	Handler         http.Handler
-	ShutdownTimeout time.Duration
-	ListenAddress   string
-	SocketConfig    listenConfig
-	TLSConfig       *tls.Config
-	HandlePanic     bool
-	Monitor         monitor
-	Logger          logger
-
-	HTTPServer httpServer
+	Context                  context.Context
+	Handler                  http.Handler
+	MaxRequestHeaderSize     int
+	ReadRequestTimeout       time.Duration
+	ReadRequestHeaderTimeout time.Duration
+	WriteResponseTimeout     time.Duration
+	IdleConnectionTimeout    time.Duration
+	ShutdownTimeout          time.Duration
+	ListenAddress            string
+	SocketConfig             listenConfig
+	TLSConfig                *tls.Config
+	HandlePanic              bool
+	Monitor                  monitor
+	Logger                   logger
+	HTTPServer               httpServer
 }
 
 func New(options ...option) ListenCloser {
@@ -53,6 +57,21 @@ func (singleton) HandlePanic(value bool) option {
 func (singleton) HTTPServer(value httpServer) option {
 	return func(this *configuration) { this.HTTPServer = value }
 }
+func (singleton) MaxRequestHeaderSize(value int) option {
+	return func(this *configuration) { this.MaxRequestHeaderSize = value }
+}
+func (singleton) ReadRequestTimeout(value time.Duration) option {
+	return func(this *configuration) { this.ReadRequestTimeout = value }
+}
+func (singleton) ReadRequestHeaderTimeout(value time.Duration) option {
+	return func(this *configuration) { this.ReadRequestHeaderTimeout = value }
+}
+func (singleton) WriteResponseTimeout(value time.Duration) option {
+	return func(this *configuration) { this.WriteResponseTimeout = value }
+}
+func (singleton) IdleConnectionTimeout(value time.Duration) option {
+	return func(this *configuration) { this.IdleConnectionTimeout = value }
+}
 func (singleton) ShutdownTimeout(value time.Duration) option {
 	return func(this *configuration) { this.ShutdownTimeout = value }
 }
@@ -77,7 +96,16 @@ func (singleton) apply(options ...option) option {
 		}
 
 		if this.HTTPServer == nil {
-			this.HTTPServer = &http.Server{Addr: this.ListenAddress, Handler: this.Handler}
+			this.HTTPServer = &http.Server{
+				Addr:              this.ListenAddress,
+				Handler:           this.Handler,
+				MaxHeaderBytes:    this.MaxRequestHeaderSize,
+				ReadTimeout:       this.ReadRequestTimeout,
+				ReadHeaderTimeout: this.ReadRequestHeaderTimeout,
+				WriteTimeout:      this.WriteResponseTimeout,
+				IdleTimeout:       this.IdleConnectionTimeout,
+				BaseContext:       func(net.Listener) context.Context { return this.Context },
+			}
 		}
 	}
 }
@@ -91,6 +119,11 @@ func (singleton) defaults(options ...option) []option {
 	return append([]option{
 		Options.ListenAddress(":http"),
 		Options.TLSConfig(nil),
+		Options.MaxRequestHeaderSize(1024 * 2),
+		Options.ReadRequestTimeout(time.Second * 5),
+		Options.ReadRequestHeaderTimeout(time.Second),
+		Options.WriteResponseTimeout(time.Second * 60),
+		Options.IdleConnectionTimeout(time.Second * 30),
 		Options.ShutdownTimeout(time.Second * 5),
 		Options.HandlePanic(true),
 		Options.Context(context.Background()),
