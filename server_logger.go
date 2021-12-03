e1a3ae041a86edfa501ded512eb7403abfdd3b4f
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2021-12-03 09:33:25 -0700

    Forwarding http.Server log messages through configured logger.

---
+++ server_logger.go
@@ -0,0 +1,31 @@
+package httpserver
+
+import (
+	"bytes"
+	"log"
+)
+
+type serverLogger struct{ logger }
+
+func newServerLogger(inner logger) *log.Logger {
+	return log.New(&serverLogger{logger: inner}, "", 0)
+}
+
+func (this *serverLogger) Write(buffer []byte) (int, error) {
+	length := len(buffer)
+
+	if length == 0 {
+		return 0, nil
+	}
+
+	buffer = buffer[0 : length-1] // trim trailing line break
+
+	if bytes.HasSuffix(buffer, ignoredLogStatements) {
+		return length, nil // no-op
+	}
+
+	this.logger.Printf("[WARN] %s", buffer)
+	return length, nil
+}
+
+var ignoredLogStatements = []byte("golang.org/issue/25192") // "http: URL query contains semicolon, which is no longer a supported separator; parts of the query may be stripped when parsed; see golang.org/issue/25192"
